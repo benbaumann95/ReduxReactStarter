fa7610122dafb41094e60168a0b5cab21dee4fdc
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-04 16:34:32 +0100

    adding boilerplate for video-detail

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,20 +3,19 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {
   // (props) => { const video = props.video; }
-  console.log(video.snippet.description);
+  const imageUrl = video.snippet.thumbnails.default.url;
   return (
     <li className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
-          <img className="media-object" />
+          <img className="media-object" src={imageUrl} />
         </div>
-
         <div className="media-body">
           <div className="media-heading">
-          </div
+            {video.snippet.title}
+          </div>
         </div>
       </div>
-      video
     </li>
   );
 };
