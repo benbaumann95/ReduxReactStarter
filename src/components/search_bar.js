5e6bdad2e4b4003720e0eb0fe9c6882beeebe2fc
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-04 13:11:32 +0100

    update input into search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 
+//class based component
 class SearchBar extends Component {
-  render() {
-    //onChange = React defined property
-    return <input onChange={event => console.log(event.target.value)} />;
+  //initialize state in a class based component (functional components do not have state)
+  constructor(props) {
+    super(props);
+
+    //create new object. Will want to record property 'term'(search) on state
+    //update it to be the value of the input
+    //only inside of our constructor function do we change our state like this
+    //everywhere else, we use this.setState, other than referencing
+    this.state = { term: '' };
   }
 
-  // event handler
-//   onInputChange(event) {
-//     console.log(event.target.value);
-//   }
-// }
+  render() {
+    //onChange = React defined property
+    return (
+      //Always wrap JS variable in {} inside of JSX
+      <div>  
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+}
+
+// event handler
+  //   onInputChange(event) {
+  //     console.log(event.target.value);
+  //   }
+  // }
 
 
 export default SearchBar;
