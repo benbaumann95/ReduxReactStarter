70dd5d3027c015a0ed7fcdf25020eadc5f6b7d4f
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-04 17:17:04 +0100

    made functioning search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -27,12 +27,18 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <div>
+      <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
+      // onChange={event => this.setState({ term: event.target.value })}
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
