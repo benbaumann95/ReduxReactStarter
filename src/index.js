b4fe2e3c2745513a108b8fa1c4351502a19543bb
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-04 12:05:54 +0100

    rendered component HTML

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-// Create a new component that should produce some HTML
+import React from 'react';
+import ReactDOM from 'react-dom'
 
+// Create a new component that should produce some HTML
 const App = () => {
   return <div>Hi!</div>;
 }
 
 
 // Take this component's generated HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+const App2 = () => {
+  const hello = <h1>"Learning React from scratch!"</h1>;
+  return hello;
+}
+
+ReactDOM.render(<App2 />, document.querySelector('.container-1'));
